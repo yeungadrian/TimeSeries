02ded7f878a180872ab9b340036f839a91dfc239
yeungadrian <yeungadrianjy@gmail.com>
2024-08-14 17:57:58 +0100

    Initial setup

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,16 +2,13 @@ import streamlit as st
 
 st.set_page_config(page_title="Time Series", page_icon="📊", layout="wide")
 
-dashboard = st.Page("pages/datasets.py", title="Datasets", icon=":material/dashboard:", default=True)
-forecasting = st.Page("pages/forecasting.py", title="Forecasting", icon=":material/bug_report:")
-probabilistic = st.Page(
-    "pages/probabilistic.py", title="Probabilistic Forecasting", icon=":material/notification_important:"
-)
+dashboard = st.Page("ui/datasets.py", title="Datasets", icon=":material/manage_search:", default=True)
+forecasting = st.Page("ui/forecasting.py", title="Forecasting", icon=":material/monitoring:")
 
 
 pg = st.navigation(
     {
-        "Time Series": [dashboard, forecasting, probabilistic],
+        "Time Series": [dashboard, forecasting],
     }
 )
 
